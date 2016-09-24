d7c1cca54b42091291172e12a4530ae7b703ea57
Alex Morega <alex@grep.ro>
2016-09-24 21:03:25 +0300

    develop against remote server

--- README.md
+++ README.md
@@ -23,3 +23,15 @@ folder. They consume the API of
 [Hoover-Search](https://github.com/hoover/search). It can serve these files
 from disk if you set `HOOVER_UI_ROOT` in `hoover/site/settings/local.py` to the
 path of the UI's `build` folder.
+
+## Development
+If you don't want to install Hoover-Search locally, you can develop against a
+remote server.
+
+```shell
+./run remotedev https://newhoover.grep.ro
+```
+
+The UI is served at `http://localhost:8000` and API calls are proxied to the
+remote server. Note that you still need to call `./run build` after every
+change.
---
+++ remotedev.js
@@ -0,0 +1,36 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let request = require('request')
+
+let remote = process.argv[2]
+let app = express()
+app.use(bodyParser.json())
+
+let proxyResponse = (res) => {
+  return (err, upstream) => {
+    if(err) { res.status(500); console.error(err); return }
+    res.send(upstream.body)
+  }
+}
+
+let proxyGet = (endpoint) => {
+  app.get(endpoint, (req, res) => {
+    let url = `${remote}${endpoint}`
+    request.get(url, {json: true}, proxyResponse(res))
+  })
+}
+
+let proxyPost = (endpoint) => {
+  app.post(endpoint, (req, res) => {
+    let url = `${remote}${endpoint}`
+    request.post(url, {json: true, body: req.body}, proxyResponse(res))
+  })
+}
+
+proxyGet('/whoami')
+proxyGet('/collections')
+proxyPost('/search')
+
+app.use(express.static(`${__dirname}/build`))
+
+app.listen(+(process.env.PORT || 8000), 'localhost')
