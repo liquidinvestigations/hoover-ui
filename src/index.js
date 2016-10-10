866df4013ffd6b513ed25593df649398cab5e67b
Alex Morega <alex@grep.ro>
2016-10-10 17:31:37 +0300

    refactor entry point to allow for `doc.html` page

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react'
 import {render} from 'react-dom'
 import SearchPage from './searchpage.js'
 
-render(<SearchPage/>, document.getElementById('app'))
+let rootNode = document.getElementById('app')
+
+let page = (name) => {
+  switch(name) {
+
+    case 'search':
+      return <SearchPage/>
+
+    default:
+      return <p>Unknown page: <tt>{name}</tt></p>
+
+  }
+}
+
+
+render(page(HOOVER_UI_CONFIG.page), rootNode)
