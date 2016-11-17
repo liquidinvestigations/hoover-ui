02cb359bd0caaac42d693c81ea9e5a542f58a16c
Dragos Catarahia <dragos@imedicare.com>
2016-11-17 16:52:30 +0200

    Added text section with expandable fade effect.

--- src/docpage.js
+++ src/docpage.js
@@ -12,71 +12,73 @@ export default class DocPage extends React.Component {
 
   render() {
     let doc = (this.state || {}).doc
-    if(! doc) return <p>loading...</p>
-
+    if(!doc) return <p></p>
 
     return (
       <div className="card doc-page">
-        <div className="card-block">
-          <p className="card-title lead">#{doc.id}: {doc.data.filename}</p>
-        </div>
-        <p className="bg-faded text-uppercase doc-section-title">
-          Meta</p>
 
-        <div className="card-block">
-          <table className="table table-sm">
-            <tbody>
-              <tr>
-                <td>Path</td>
-                <td><code>{doc.data.path}</code></td>
-              </tr>
-              <tr>
-                <td>Filename</td>
-                <td><code>{doc.data.filename}</code></td>
-              </tr>
-              {doc.data.type &&
-                <tr>
-                  <td>Type</td>
-                  <td><code>{doc.data.type}</code></td>
-                </tr>
-              }
-              {doc.data.type != 'folder' && doc.data.md5 &&
-                <tr>
-                  <td>MD5</td>
-                  <td><code>{doc.data.md5}</code></td>
-                </tr>
-              }
-              {doc.data.type != 'folder' && doc.data.sha1 &&
-                <tr>
-                  <td>SHA1</td>
-                  <td><code>{doc.data.sha1}</code></td>
-                </tr>
-              }
-              {doc.data.lang &&
-                <tr>
-                  <td>Language</td>
-                  <td><code>{doc.data.lang}</code></td>
-                </tr>
-              }
-              {doc.data['date-created'] &&
-                <tr>
-                  <td>Created</td>
-                  <td>{doc.data['date-created']}</td>
-                </tr>
-              }
-              {doc.data.date &&
-                <tr>
-                  <td>Modified</td>
-                  <td>{doc.data.date}</td>
-                </tr>
-              }
-            </tbody>
-          </table>
+        <div className="lead">#{doc.id}: {doc.data.filename}</div>
 
-        </div>
+        <div className="bg-faded doc-section-title">Meta</div>
+
+        <table className="table table-sm">
+          <tbody>
+            <tr>
+              <td style={{width: 110}}>Path</td>
+              <td><code>{doc.data.path}</code></td>
+            </tr>
+            <tr>
+              <td>Filename</td>
+              <td><code>{doc.data.filename}</code></td>
+            </tr>
+            {doc.data.type &&
+              <tr>
+                <td>Type</td>
+                <td><code>{doc.data.type}</code></td>
+              </tr>
+            }
+            {doc.data.type != 'folder' && doc.data.md5 &&
+              <tr>
+                <td>MD5</td>
+                <td><code>{doc.data.md5}</code></td>
+              </tr>
+            }
+            {doc.data.type != 'folder' && doc.data.sha1 &&
+              <tr>
+                <td>SHA1</td>
+                <td><code>{doc.data.sha1}</code></td>
+              </tr>
+            }
+            {doc.data.lang &&
+              <tr>
+                <td>Language</td>
+                <td><code>{doc.data.lang}</code></td>
+              </tr>
+            }
+            {doc.data['date-created'] &&
+              <tr>
+                <td>Created</td>
+                <td>{doc.data['date-created']}</td>
+              </tr>
+            }
+            {doc.data.date &&
+              <tr>
+                <td>Modified</td>
+                <td>{doc.data.date}</td>
+              </tr>
+            }
+            {doc.data.pgp &&
+              <tr>
+                <td>PGP</td>
+                <td>{doc.data.pgp}</td>
+              </tr>
+            }
+          </tbody>
+        </table>
 
         <DocEmailSection doc={doc} />
         <DocFilesSection title="Files" data={doc.data.files || []} />
+        <DocTextSection doc={doc} />
 
       </div>
     )
@@ -99,17 +101,15 @@ class DocEmailSection extends React.Component {
 
     return doc.data.type == 'email' && (
       <div>
-        <p className="bg-faded text-uppercase doc-section-title">Email</p>
-        <div className="card-block">
-          <table className="table table-sm">
-            <tbody>
-              <tr><td>From</td> <td>{doc.data.from}</td></tr>
-              <tr><td>To</td> <td>{doc.data.to.join(', ')}</td></tr>
-              <tr><td>Date</td> <td>{doc.data.date}</td></tr>
-              <tr><td>Subject</td> <td>{doc.data.subject || '---'}</td></tr>
-            </tbody>
-          </table>
-        </div>
+        <p className="bg-faded doc-section-title">Email</p>
+        <table className="table table-sm">
+          <tbody>
+            <tr><td>From</td> <td>{doc.data.from}</td></tr>
+            <tr><td>To</td> <td>{doc.data.to.join(', ')}</td></tr>
+            <tr><td>Date</td> <td>{doc.data.date}</td></tr>
+            <tr><td>Subject</td> <td>{doc.data.subject || '---'}</td></tr>
+          </tbody>
+        </table>
         <DocFilesSection title="Attachments" data={files} />
       </div>
     )
@@ -144,12 +144,38 @@ class DocFilesSection extends React.Component {
 
     return files.length > 0 && (
       <div>
-        <p className="bg-faded text-uppercase doc-section-title">
+        <p className="bg-faded doc-section-title">
           {this.props.title}</p>
-        <div className="card-block">
-          <table className="table table-sm">
-            <tbody>{files}</tbody>
-          </table>
+        <table className="table table-sm">
+          <tbody>{files}</tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+
+class DocTextSection extends React.Component {
+
+  render() {
+    let doc = this.props.doc
+    if(!doc.data.text) return
+    let expanded = (this.state || {}).expanded
+    let data = doc.data.text.replace(/ /g,'');
+    if(data.length <= 700) {
+      expanded = true
+    }
+
+    return (
+      <div>
+        <div className="bg-faded doc-section-title">Text</div>
+        <div className="content">
+          {expanded ? <pre>{data}</pre> :
+           <pre className="content-wrap"
+                onClick={() => {this.setState({expanded: true})}}>
+              {data}
+            </pre>
+          }
         </div>
       </div>
     )
