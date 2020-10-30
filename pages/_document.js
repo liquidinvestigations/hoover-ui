f541dfc4af0339a0f0bf8899c0aa90b54ce3a5d2
Mati <mateusz.szczygielski@gmail.com>
2020-10-30 17:22:22 +0100

    add server side generated styles

---
+++ pages/_document.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import { removeCommentsAndSpacing } from "../src/utils";
+import { JSS_CSS } from "../src/constants";
+
+class HooverDocument extends Document {
+    static async getInitialProps(ctx) {
+        // Render app and page and get the context of the page with collected side effects.
+        const sheets = new ServerStyleSheets();
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            // Styles fragment is rendered after the app and page rendering finish.
+            styles: [
+                ...React.Children.toArray(initialProps.styles),
+                <style
+                    id={JSS_CSS}
+                    key={JSS_CSS}
+                    dangerouslySetInnerHTML={{
+                        __html: removeCommentsAndSpacing(sheets.toString())
+                    }}
+                />
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default HooverDocument
