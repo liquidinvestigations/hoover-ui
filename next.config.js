20dc3d3b9663f06f6de80f6432810b6dd6407dd6
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2021-03-10 09:43:20 +0200

    swap api/v0 for api/v1

--- next.config.js
+++ next.config.js
@@ -45,33 +45,33 @@ module.exports = withPlugins([ withTM ],
         compress: false,
         redirects: () => [{
             source: '/doc/:collection/:id/raw/:filename',
-            destination: '/api/v0/doc/:collection/:id/raw/:filename',
+            destination: '/api/v1/doc/:collection/:id/raw/:filename',
             permanent: true,
         }],
         rewrites: () => REWRITE_API ? [{
-            source: '/api/v0/whoami',
-            destination: API_URL + '/api/v0/whoami',
+            source: '/api/v1/whoami',
+            destination: API_URL + '/api/v1/whoami',
         },{
-            source: '/api/v0/batch',
-            destination: API_URL + '/api/v0/batch',
+            source: '/api/v1/batch',
+            destination: API_URL + '/api/v1/batch',
         },{
-            source: '/api/v0/doc/:collection/:hash/json',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/json',
+            source: '/api/v1/doc/:collection/:hash/json',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/json',
         },{
-            source: '/api/v0/doc/:collection/:hash/locations',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/locations',
+            source: '/api/v1/doc/:collection/:hash/locations',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/locations',
         },{
-            source: '/api/v0/doc/:collection/:hash/tags',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/tags',
+            source: '/api/v1/doc/:collection/:hash/tags',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/tags',
         },{
-            source: '/api/v0/doc/:collection/:hash/tags/:id',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/tags/:id',
+            source: '/api/v1/doc/:collection/:hash/tags/:id',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/tags/:id',
         },{
-            source: '/api/v0/doc/:collection/:hash/raw/:filename',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/raw/:filename',
+            source: '/api/v1/doc/:collection/:hash/raw/:filename',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/raw/:filename',
         },{
-            source: '/api/v0/doc/:collection/:hash/ocr/:tag',
-            destination: API_URL + '/api/v0/doc/:collection/:hash/ocr/:tag',
+            source: '/api/v1/doc/:collection/:hash/ocr/:tag',
+            destination: API_URL + '/api/v1/doc/:collection/:hash/ocr/:tag',
         }] : [],
     }
 )
