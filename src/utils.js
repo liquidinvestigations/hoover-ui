a39fcdfd66c509f473411ccea462af7071133d81
Mateusz Szczygielski <mateusz.szczygielski@gmail.com>
2021-08-25 14:33:05 +0200

    flatten meta objects to dot separated keys and newlines

--- src/utils.js
+++ src/utils.js
@@ -197,3 +197,21 @@ export const formatTitleCase = name => {
         </>
     )
 }
+
+// $roots keeps previous parent properties as they will be added as a prefix for each prop.
+// $sep is just a preference if you want to seperate nested paths other than dot.
+export const flatten = (obj, roots = [], sep = '.') => Object
+    // find props of given object
+    .keys(obj)
+    // return an object by iterating props
+    .reduce((memo, prop) => Object.assign(
+        // create a new object
+        {},
+        // include previously returned object
+        memo,
+        Object.prototype.toString.call(obj[prop]) === '[object Object]'
+            // keep working if value is an object
+            ? flatten(obj[prop], roots.concat([prop]))
+            // include current prop and value and prefix prop with the roots
+            : {[roots.concat([prop]).join(sep)]: obj[prop]}
+    ), {})
