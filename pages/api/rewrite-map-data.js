d7b888928634802c6686baf8cdd766eb96be90a9
Mati <mateusz.szczygielski@gmail.com>
2022-02-22 18:06:19 +0100

    change map data rewrite env var

--- pages/api/rewrite-map-data.js
+++ pages/api/rewrite-map-data.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const { API_URL, REWRITE_API } = process.env
+const { HOOVER_URL, REWRITE_API } = process.env
 
 const handler = async (req, res) => {
     try {
         const data = fs.readFileSync('./src/constants/map-data-v3.json')
-        res.json(data.toString().replace('local:', REWRITE_API ? 'http://localhost:8000' : API_URL))
+        res.json(data.toString().replace('local:', REWRITE_API ? 'http://localhost:8000' : HOOVER_URL))
         res.end()
 
     } catch (e) {
