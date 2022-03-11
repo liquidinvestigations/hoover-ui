3f2a8b200715f3956ed7466e1b76a56c7ea1f20e
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-03-11 08:13:26 +0200

    fix insights task table column list

--- src/components/insights/TaskTable.js
+++ src/components/insights/TaskTable.js
@@ -20,15 +20,21 @@ export default function TaskTable({ tasks }) {
                 <TableHead>
                     <TableRow>
                         <TableCell>Func</TableCell>
-                        <TableCell align="right">Pending</TableCell>
-                        <TableCell align="right">Deferred</TableCell>
-                        <TableCell align="right">Success</TableCell>
-                        <TableCell align="right">Broken</TableCell>
-                        <TableCell align="right">Error</TableCell>
-                        <TableCell align="right">5M</TableCell>
-                        <TableCell align="right">5M Duration</TableCell>
-                        <TableCell align="right">5M Fill</TableCell>
                         <TableCell align="right">ETA</TableCell>
+                        <TableCell align="right">ERROR</TableCell>
+                        <TableCell align="right">BROKEN</TableCell>
+                        <TableCell align="right">PENDING</TableCell>
+                        <TableCell align="right">SUCCESS</TableCell>
+                        <TableCell align="right">DEFERRED</TableCell>
+                        <TableCell align="right">5M_COUNT</TableCell>
+                        <TableCell align="right">REMAINING_SIZE</TableCell>
+                        <TableCell align="right">5M_AVG_SIZE</TableCell>
+                        <TableCell align="right">5M_AVG_WORKERS</TableCell>
+                        <TableCell align="right">5M_AVG_DURATION</TableCell>
+                        <TableCell align="right">SUCCESS_AVG_SIZE</TableCell>
+                        <TableCell align="right">SUCCESS_AVG_DURATION</TableCell>
+                        <TableCell align="right">5M_AVG_BYTES_SEC</TableCell>
+                        <TableCell align="right">SUCCESS_AVG_BYTES_SEC</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -37,15 +43,21 @@ export default function TaskTable({ tasks }) {
                             <TableCell>
                                 {formatTitleCase(task)}
                             </TableCell>
-                            <TableCell align="right">{status.pending}</TableCell>
-                            <TableCell align="right">{status.deferred}</TableCell>
-                            <TableCell align="right">{status.success}</TableCell>
-                            <TableCell align="right">{status.broken}</TableCell>
-                            <TableCell align="right">{status.error}</TableCell>
-                            <TableCell align="right">{status['5m']}</TableCell>
-                            <TableCell align="right">{status['5m_duration']}</TableCell>
-                            <TableCell align="right">{status['5m_fill']}</TableCell>
-                            <TableCell align="right">{status.eta}</TableCell>
+                            <TableCell align="right">{status['eta']}</TableCell>
+                            <TableCell align="right">{status['error']}</TableCell>
+                            <TableCell align="right">{status['broken']}</TableCell>
+                            <TableCell align="right">{status['pending']}</TableCell>
+                            <TableCell align="right">{status['success']}</TableCell>
+                            <TableCell align="right">{status['deferred']}</TableCell>
+                            <TableCell align="right">{status['5m_count']}</TableCell>
+                            <TableCell align="right">{status['remaining_size']}</TableCell>
+                            <TableCell align="right">{status['5m_avg_size']}</TableCell>
+                            <TableCell align="right">{status['5m_avg_workers']}</TableCell>
+                            <TableCell align="right">{status['5m_avg_duration']}</TableCell>
+                            <TableCell align="right">{status['success_avg_size']}</TableCell>
+                            <TableCell align="right">{status['success_avg_duration']}</TableCell>
+                            <TableCell align="right">{status['5m_avg_bytes_sec']}</TableCell>
+                            <TableCell align="right">{status['success_avg_bytes_sec']}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
