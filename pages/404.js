a4f7780d3da0caa22064d6e2d89fe8e2d8035d24
Mateusz Szczygielski <mszczygielski@onairent.live>
2023-02-10 15:31:24 +0100

    remove unnecessary imports

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Error from './_error'
 
 export default function Custom404() {
--- pages/upload/[collection]/[id].js
+++ pages/upload/[collection]/[id].js
@@ -1,26 +1,9 @@
-import React from 'react'
 import { useRouter } from 'next/router'
-import { SearchProvider } from '../../../src/components/search/SearchProvider'
-import getAuthorizationHeaders from '../../../src/backend/getAuthorizationHeaders'
-import { collections as collectionsAPI } from '../../../src/backend/api'
+
 import DirectoryUploads from '../../../src/components/uploads/DirectoryUploads'
 
-export default function DirectoryUploadsPage({ collections, serverQuery }) {
+export default function DirectoryUploadsPage() {
+    const { query } = useRouter()
 
-    const router = useRouter()
-    const { query } = router
-    const printMode = query.print && query.print !== 'false'
-
-    return (
-            <DirectoryUploads collection={query.collection} directoryId={query.id} />
-    )
-}
-
-export async function getServerSideProps({ req }) {
-    const headers = getAuthorizationHeaders(req)
-    const collections = await collectionsAPI(headers)
-
-    const serverQuery = req.url.split('?')[1]?.split('#')[0] || ''
-
-    return { props: { collections, serverQuery }}
+    return <DirectoryUploads collection={query.collection} directoryId={query.id} />
 }
