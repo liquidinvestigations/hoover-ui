4999171583f7a54069c70d2c8c66b06b28cc1f95
Daniel Kuti <danielkuti09@gmail.com>
2023-03-23 15:16:06 +0200

    [EIC-994]: fixed add tags in document

---
+++ src/constants/autocomplete.ts
@@ -0,0 +1,7 @@
+export const AUTOCOMPLETE_CHANGE_REASON = {
+    CREATE_OPTION: 'createOption',
+    SELECT_OPTION: 'selectOption',
+    REMOVE_OPTION: 'removeOption',
+    CLEAR: 'clear',
+    BLUR: 'blur',
+}
