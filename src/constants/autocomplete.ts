c46fdc3e8e1985dc6041eae15bf5440f1cb2b837
Daniel Kuti <danielkuti09@gmail.com>
2023-04-10 12:40:45 +0300

    [EIC-994]: removed AutocompleteChangeReason constant

--- src/constants/autocomplete.ts
+++
@@ -1,7 +0,0 @@
-export const AUTOCOMPLETE_CHANGE_REASON = {
-    CREATE_OPTION: 'createOption',
-    SELECT_OPTION: 'selectOption',
-    REMOVE_OPTION: 'removeOption',
-    CLEAR: 'clear',
-    BLUR: 'blur',
-}
