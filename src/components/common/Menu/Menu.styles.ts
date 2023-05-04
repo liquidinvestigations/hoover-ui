905610eafbd3ae706a20c7301c0c5871190c4675
Daniel Kuti <danielkuti09@gmail.com>
2023-05-04 15:19:37 +0300

    [EIC-996]: fixed responsive Navbar

--- src/components/common/Menu/Menu.styles.ts
+++ src/components/common/Menu/Menu.styles.ts
@@ -1,6 +1,13 @@
 import { makeStyles } from 'tss-react/mui'
 
-export const useStyles = makeStyles()(() => ({
+export const useStyles = makeStyles()((theme) => ({
+    menuHeader: {
+        padding: `0 ${theme.spacing(2)}`,
+    },
+    menuItem: {
+        color: 'black',
+        textDecoration: 'none',
+    },
     link: {
         color: 'white',
         textDecoration: 'none',
--- src/components/common/Menu/Menu.tsx
+++ src/components/common/Menu/Menu.tsx
@@ -1,8 +1,10 @@
-import { Button } from '@mui/material'
+import { Button, IconButton, MenuItem, Menu as MenuMui, Typography, Divider, Box, MenuList } from '@mui/material'
 import { observer } from 'mobx-react-lite'
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
+import { reactIcons } from '../../../constants/icons'
 import { useSharedStore } from '../../SharedStoreProvider'
 
 import { useStyles } from './Menu.styles'
@@ -15,6 +17,7 @@ interface Link {
 }
 
 export const Menu = observer(() => {
+    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
     const { classes } = useStyles()
     const router = useRouter()
     const { user } = useSharedStore()
@@ -26,73 +29,65 @@ export const Menu = observer(() => {
         return null
     }
 
-    const links: Link[] = (
-        [
-            {
-                name: 'Search',
-                url: '/',
-                next: true,
-            },
-            {
-                name: 'Batch search',
-                url: '/batch-search',
-                next: true,
-            },
-            {
-                name: 'Insights',
-                url: '/insights',
-                next: true,
-            },
-            process.env.HOOVER_UPLOADS_ENABLED
-                ? {
-                      name: 'Uploads',
-                      url: '/uploads',
-                      next: true,
-                  }
-                : false,
-            process.env.HOOVER_MAPS_ENABLED
-                ? {
-                      name: 'Maps',
-                      url: '/maps',
-                      next: true,
-                  }
-                : false,
-            process.env.HOOVER_TRANSLATION_ENABLED
-                ? {
-                      name: 'Translate',
-                      url: '/libre_translate',
-                  }
-                : false,
-            {
-                name: 'About',
-                url: 'https://github.com/liquidinvestigations/hoover-search',
-            },
-            //{
-            //    name: 'Terms',
-            //    url: '/terms',
-            //    next: true,
-            //},
-            {
-                name: 'Documentation',
-                url: 'https://github.com/liquidinvestigations/docs/wiki/User-Guide:-Hoover',
-            },
-            {
-                name: 'Login',
-                url: user.urls.login,
-                type: 'not-logged-in',
-            },
-            {
-                name: 'Admin',
-                url: user.urls.admin,
-                type: 'admin',
-            },
-            {
-                name: `Logout (${user.username})`,
-                url: user.urls.logout,
-                type: 'logged-in',
-            },
-        ].filter(Boolean) as Link[]
-    ).map((link) => ({ ...link, active: router.asPath === link.url }))
+    const links: Link[] = ([
+        {
+            name: 'Search',
+            url: '/',
+            next: true,
+        },
+        {
+            name: 'Batch',
+            url: '/batch-search',
+            next: true,
+        },
+        {
+            name: 'Insights',
+            url: '/insights',
+            next: true,
+        },
+        process.env.HOOVER_UPLOADS_ENABLED
+            ? {
+                  name: 'Uploads',
+                  url: '/uploads',
+                  next: true,
+              }
+            : false,
+        process.env.HOOVER_MAPS_ENABLED
+            ? {
+                  name: 'Maps',
+                  url: '/maps',
+                  next: true,
+              }
+            : false,
+        process.env.HOOVER_TRANSLATION_ENABLED
+            ? {
+                  name: 'Translate',
+                  url: '/libre_translate',
+              }
+            : false,
+        {
+            name: 'Docs',
+            url: 'https://github.com/liquidinvestigations/docs/wiki/User-Guide:-Hoover',
+        },
+    ].filter(Boolean) as Link[]).map((link: Link) => ({ ...link, active: router.asPath === link.url }))
+
+    const userMenuLinks: Link[] = [
+        {
+            name: 'Login',
+            url: user.urls.login,
+            type: 'not-logged-in',
+        },
+        {
+            name: 'Admin',
+            url: user.urls.admin,
+            type: 'admin',
+        },
+        {
+            name: `Logout`,
+            url: user.urls.logout,
+            type: 'logged-in',
+        },
+    ]
 
     const shouldShow = (link: Link) => {
         if (link.type === 'admin') {
@@ -107,21 +102,47 @@ export const Menu = observer(() => {
         return true
     }
 
+    const handleUserMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleUserMenuClose = () => {
+        setAnchorEl(null)
+    }
+
     return (
         <>
-            {links.filter(shouldShow).map((link) =>
+            {links.map((link) =>
                 !link.next ? (
                     <Button key={link.name} variant="text" href={link.url} color="inherit">
                         {link.name}
                     </Button>
                 ) : (
-                    <Link key={link.name} href={link.url} shallow className={classes.link}>
+                    <NextLink key={link.name} href={link.url} shallow className={classes.link}>
                         <Button variant="text" href={link.url} color="inherit" component="span">
                             {link.name}
                         </Button>
-                    </Link>
+                    </NextLink>
                 )
             )}
+            <IconButton edge="end" color="inherit" onClick={handleUserMenuClick}>
+                {reactIcons.accountCircle}
+            </IconButton>
+            <MenuMui anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleUserMenuClose}>
+                <Box className={classes.menuHeader}>
+                    <Typography variant="subtitle1">{user.username}</Typography>
+                </Box>
+                <Divider />
+                <MenuList>
+                    {userMenuLinks.filter(shouldShow).map((link) => (
+                        <MenuItem key={link.name}>
+                            <NextLink href={link.url} shallow className={classes.menuItem}>
+                                {link.name}
+                            </NextLink>
+                        </MenuItem>
+                    ))}
+                </MenuList>
+            </MenuMui>
         </>
     )
 })
