b0f70f350e07dfcd0beb3c5f5ad415da823e181d
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-07-07 17:47:18 +0300

    sentry config - enable debug mode

--- next.config.js
+++ next.config.js
@@ -202,7 +202,7 @@ module.exports = withSentryConfig(
     transpileClientSDK: true,
 
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/sentry-monitoring",
+    // tunnelRoute: "/sentry-monitoring",
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
