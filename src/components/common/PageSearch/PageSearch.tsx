7ccf77a1d372ccdde6ade8c36f3e03a41f8b2edf
danielkuti09 <44607878+danielkuti09@users.noreply.github.com>
2023-08-29 17:00:26 +0300

    [EIC-1094]: added chips for prefilling document search query (#364)

--- src/components/common/PageSearch/PageSearch.tsx
+++ src/components/common/PageSearch/PageSearch.tsx
@@ -13,6 +13,7 @@ import { useStyles } from './PageSearch.styles'
 export const PageSearch = observer(() => {
     const { classes } = useStyles()
     const {
+        searchStore: { query },
         documentStore: {
             documentSearchStore: { inputValue, setInputValue, activeSearch, clearQuery },
         },
@@ -27,42 +28,57 @@ export const PageSearch = observer(() => {
         clearQuery()
     }
 
+    const handleChipClick = (chip: string) => {
+        if (inputValue && !inputValue.endsWith(' ')) {
+            setInputValue(`${inputValue} ${chip}`);
+        } else {
+            setInputValue(chip);
+        }
+    }
+    
     return (
-        <TextField
-            autoComplete="off"
-            sx={{ display: 'block' }}
-            id="standard-basic"
-            variant="standard"
-            className={classes.input}
-            type="text"
-            placeholder="Search..."
-            value={inputValue}
-            onChange={handleInputChange}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon />
-                    </InputAdornment>
-                ),
-                endAdornment: (
-                    <InputAdornment position="end">
-                        {inputValue && activeSearch.getSearchResultsCount() > 0 && (
-                            <Box className={classes.searchCount}>
-                                {activeSearch.getCurrentHighlightIndex() + 1} of {activeSearch.getSearchResultsCount()}
-                            </Box>
-                        )}
-                        <IconButton onClick={handleClearInput}>
-                            <CloseIcon />
-                        </IconButton>
-                        <IconButton onClick={activeSearch.nextSearchResult}>
-                            <ArrowDownwardIcon />
-                        </IconButton>
-                        <IconButton onClick={activeSearch.previousSearchResult}>
-                            <ArrowUpwardIcon />
-                        </IconButton>
-                    </InputAdornment>
-                ),
-            }}
-        />
+        <>
+            {query?.q.split(' ').filter((chip) => !inputValue.includes(chip)).map((chip) => (
+                <Box key={chip} className={classes.chip} onClick={() => handleChipClick(chip)}>
+                    {chip}
+                </Box>
+            ))}
+            <TextField
+                autoComplete="off"
+                sx={{ display: 'block' }}
+                id="standard-basic"
+                variant="standard"
+                className={classes.input}
+                type="text"
+                placeholder="Search..."
+                value={inputValue}
+                onChange={handleInputChange}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            {inputValue && activeSearch.getSearchResultsCount() > 0 && (
+                                <Box className={classes.searchCount}>
+                                    {activeSearch.getCurrentHighlightIndex() + 1} of {activeSearch.getSearchResultsCount()}
+                                </Box>
+                            )}
+                            <IconButton onClick={handleClearInput}>
+                                <CloseIcon />
+                            </IconButton>
+                            <IconButton onClick={activeSearch.nextSearchResult}>
+                                <ArrowDownwardIcon />
+                            </IconButton>
+                            <IconButton onClick={activeSearch.previousSearchResult}>
+                                <ArrowUpwardIcon />
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </>
     )
 })
