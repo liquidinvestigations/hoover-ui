a5f50e0f879e28e27b957f72d8d6987ec4d20871
Daniel Kuti <danielkuti09@gmail.com>
2024-02-09 10:58:53 +0200

    Added unit tests for Text component

---
+++ src/components/document/SubTabs/components/Text/Text.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithProviders } from '../../../../../../__test__/jest.utils'
+
+import { Text } from './Text'
+import '@testing-library/jest-dom'
+
+describe('Text Component', () => {
+    it('renders "No text" when content is empty', async () => {
+        renderWithProviders(<Text content="" />)
+        const noTextElement = await screen.findByText('No text')
+        expect(noTextElement).toBeInTheDocument()
+    })
+
+    it('renders content when it is provided', () => {
+        const testContent = 'Test content'
+        renderWithProviders(<Text content={testContent} />)
+        const contentElement = screen.getByText(testContent)
+        expect(contentElement).toBeInTheDocument()
+    })
+
+    test('escapes HTML special characters in content', () => {
+        const unsafeContent = '<script>alert("XSS")</script>'
+        const escapedContent = '&lt;script&gt;alert("XSS")&lt;/script&gt;'
+        const { container } = renderWithProviders(<Text content={unsafeContent} />)
+        expect(container.children[0].innerHTML).toContain(escapedContent)
+        expect(container.innerHTML).not.toContain(unsafeContent)
+    })
+})
