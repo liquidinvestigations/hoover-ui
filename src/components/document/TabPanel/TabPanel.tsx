192b5b65e388b413bef4400d16b46a79d8129c78
Daniel Kuti <danielkuti09@gmail.com>
2024-03-20 17:25:54 +0200

    removed print mode feature & code cleanup

--- src/components/document/TabPanel/TabPanel.tsx
+++ src/components/document/TabPanel/TabPanel.tsx
@@ -1,28 +1,11 @@
-import { Box, Typography } from '@mui/material'
-import { FC, ReactElement, ReactNode } from 'react'
+import { Box } from '@mui/material'
+import { FC } from 'react'
 
 import { useStyles } from './TabPanel.styles'
+import { TabPanelProps } from './TabPanel.types'
 
-interface TabPanelProps {
-    children: ReactNode | ReactNode[]
-    value: number
-    index: number
-    padding?: number
-    alwaysVisible?: boolean
-    name?: string | ReactElement
-}
-
-export const TabPanel: FC<TabPanelProps> = ({ children, value, index, padding = 2, alwaysVisible, name }) => {
+export const TabPanel: FC<TabPanelProps> = ({ children, value, index, padding = 2 }) => {
     const { classes } = useStyles({ isHidden: index !== value, padding })
 
-    return (
-        <Box className={classes.root}>
-            {alwaysVisible && (
-                <Typography variant="h5" className={classes.printTitle}>
-                    {name}
-                </Typography>
-            )}
-            {children}
-        </Box>
-    )
+    return <Box className={classes.root}>{children}</Box>
 }
